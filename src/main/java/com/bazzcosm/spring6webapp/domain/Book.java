273a0abb4129adafa235237bd82fe8fd69070e37
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2025-04-19 01:06:28 +0200

    Add POJOs in domain for Author and Book entity

---
+++ src/main/java/com/bazzcosm/spring6webapp/domain/Book.java
@@ -0,0 +1,40 @@
+package com.bazzcosm.spring6webapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String title;
+    private String isbn;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
