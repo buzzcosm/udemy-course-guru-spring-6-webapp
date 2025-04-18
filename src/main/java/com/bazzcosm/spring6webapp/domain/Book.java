43fb3f8d5c3a8b4d8d7e1cdf73498a4a54a48efc
Jeff_Buzz <97390468+buzzcosm@users.noreply.github.com>
2025-04-19 01:27:12 +0200

    Generate methods for 'toString', 'equals' and 'hashCode' from IntelliJ Idea

--- src/main/java/com/bazzcosm/spring6webapp/domain/Book.java
+++ src/main/java/com/bazzcosm/spring6webapp/domain/Book.java
@@ -2,6 +2,7 @@ package com.bazzcosm.spring6webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -51,5 +52,28 @@ public class Book {
 
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return Objects.equals(getId(), book.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
     }
 }
